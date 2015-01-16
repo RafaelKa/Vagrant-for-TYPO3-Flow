326c7e291e11b63acf5b4e965ea51d4d77f7c9bf
Rafael Kähm <rafael@kaehms.de>
2015-01-17 00:59:57 +0100

    Mac Style? WTF?

--- puppet/manifests/trusty.pp
+++ puppet/manifests/trusty.pp
@@ -8,12 +8,12 @@ Exec {
   path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]
 }
 
-# Use Mac style installation folder
-file { "/usr/local/bin":
-  owner => "vagrant",
-  group => "vagrant",
-  recurse => true
-}
+## Use Mac style installation folder
+#file { "/usr/local/bin":
+#  owner => "vagrant",
+#  group => "vagrant",
+#  recurse => true
+#}
 
 # Strangely, bash is not the default...
 user { "vagrant":
