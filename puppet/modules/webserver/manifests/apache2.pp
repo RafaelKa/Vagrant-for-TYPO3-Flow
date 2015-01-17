50375ba196643848359e260414010e799977d432
Rafael Kähm <rafael@kaehms.de>
2015-01-17 01:57:17 +0100

    enable mod rewrite for apache

--- puppet/modules/webserver/manifests/apache2.pp
+++ puppet/modules/webserver/manifests/apache2.pp
@@ -9,7 +9,15 @@ class webserver::apache2 {
 
   exec {'enabling mod_proxy_fcgi':
     command => 'a2enmod proxy_fcgi',
-#    subscribe => Package['apache2'],
+    require => Package['apache2']
+  }
+  ->
+  notify { 'enable mod_proxy_fcgi':
+    message => 'mod_proxy_fcgi enabled'
+  }
+
+  exec {'enabling mod_rewrite':
+    command => 'a2enmod rewrite',
     require => Package['apache2']
   }
   ->
