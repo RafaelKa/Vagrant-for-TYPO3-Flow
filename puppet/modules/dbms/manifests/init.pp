076abb16e582ff2f884da66132e21d25d5c89ff6
Rafael Kähm <rafael@kaehms.de>
2015-01-17 01:37:08 +0100

    MongoDB entfernt

--- puppet/modules/dbms/manifests/init.pp
+++ puppet/modules/dbms/manifests/init.pp
@@ -1,9 +1,9 @@
 class dbms {
 
 
-#  include dbms::mysql
+  include dbms::mysql
 #  include dbms::postgresql
-#  include dbms::couchdb
-  include dbms::mongodb
+  include dbms::couchdb
+#  include dbms::mongodb
 
 }
