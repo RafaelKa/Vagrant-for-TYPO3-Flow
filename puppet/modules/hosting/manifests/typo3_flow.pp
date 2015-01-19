ad982c03282bcee07073fd0ea66ce28ac2f01f51
Rafael Kähm <rafael@kaehms.de>
2015-01-19 03:03:21 +0100

    Issues by installation from TYPO3 Flow fixed

--- puppet/modules/hosting/manifests/typo3_flow.pp
+++ puppet/modules/hosting/manifests/typo3_flow.pp
@@ -1,8 +1,10 @@
 class hosting::typo3_flow {
 
   exec { 'install-typo3-flow':
-  command => 'composer create-project --dev --keep-vcs typo3/flow-base-distribution /var/www/projects/typo3-flow.dev',
-  require => [ Exec['composer-install'], File["/var/www/projects"] ]
+  command => 'su --login --shell /bin/bash --command "composer create-project --dev --keep-vcs typo3/flow-base-distribution /var/www/projects/typo3-flow.dev" typo3',
+  require => [ User['typo3'], Exec['composer-install'], File['/var/www/projects'], Package['git'] ],
+  returns => [0, 1, 2, 14]
+
   }
   ->
   notify { 'typo3-flow-is-installed':
