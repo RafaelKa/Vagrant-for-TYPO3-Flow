a0214f4f31ce7440ffdb0a5781267ff2754b9666
Rafael Kähm <rafael@kaehms.de>
2015-01-19 03:39:55 +0100

    Set timeout 600 for TYPO3 Flow installation

--- puppet/modules/hosting/manifests/typo3_flow.pp
+++ puppet/modules/hosting/manifests/typo3_flow.pp
@@ -3,7 +3,8 @@ class hosting::typo3_flow {
   exec { 'install-typo3-flow':
   command => 'su --login --shell /bin/bash --command "composer create-project --dev --keep-vcs typo3/flow-base-distribution /var/www/projects/typo3-flow.dev" typo3',
   require => [ User['typo3'], Exec['composer-install'], File['/var/www/projects'], Package['git'] ],
-  returns => [0, 1, 2, 14]
+#  returns => [0, 1, 2, 14]
+  timeout => 600
 
   }
   ->
